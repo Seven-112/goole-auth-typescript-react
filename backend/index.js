d36ae866f5abd1206a69a43c0c937954fc231eeb
Mouthwatering777 <bluesatellite777@gmail.com>
2022-07-30 09:54:30 -0700

    passport

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,27 @@
 const express = require('express');
 const path = require('path');
-const session = require('express-session');
+const expressSession = require('express-session');
+require('dotenv').config();
 
 const app = express();
 
-const port = process.env.PORT || '8000';
+const port = process.env.PORT || '3000';
 
 app.set('view engine', 'ejs');
 
-app.use(session({
+const session = {
   resave: false,
   saveUninitialized: true,
-  secret: 'SECRET'
-}))
+  secret: process.env.SESSION_SECRET,
+  cookie: {}
+}
+
+if(app.get('env') === 'production') {
+  session.cookie.secure = true;
+}
+
+
+app.use(expressSession(session));
 
 app.get('/', (req, res) => {
   //res.status(200).send("Hello");
@@ -30,7 +39,7 @@ const { ppid } = require('process');
 var userProfile;
 
 app.use(passport.initialize());
-app.use(passport.session);
+app.use(passport.session());
 
 app.set('view engine', 'ejs');
 
@@ -53,21 +62,20 @@ passport.deserializeUser(function (obj, cb) {
 /* Google AUTH */
 
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-const GOOGLE_CLIENT_ID = 'our-google-client-id';
-const GOOGLE_CLIENT_SECRET = 'our-google-client-secret';
+const GOOGLE_CLIENT_ID = '637870408143-9rh4k945lfur8ptpv5q3n5h54gvfr6oh.apps.googleusercontent.com';
+const GOOGLE_CLIENT_SECRET = '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';
 
 passport.use(new GoogleStrategy({
-  clientID: GOOGLE_CLIENT_ID,
-  clientSecret: GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/google/callback"
-},
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/callback"
+  },
   function (accessToken, refreshToken, profile, done) {
     userProfile = profile;
     return done(null, userProfile);
   }
 ));
-app.get('/auth/google',
-  passport.authenticate('google', { scope: ['profile', 'email'] }));
+app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
 
 app.get('/auth/google/callback',
   passport.authenticate('google', { failureRedirect: '/error' }),
