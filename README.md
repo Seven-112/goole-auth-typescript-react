b22e2acfceea45bc28c2626751f376bb22d38970
Mouthwatering777 <bluesatellite777@gmail.com>
2022-07-30 08:20:44 -0700

    second

--- README.md
+++
@@ -1,2 +0,0 @@
-# goole-auth-typescript-react
- 
--- backend/index.js
+++ backend/index.js
@@ -11,4 +11,4 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Listening to requests on http://localhost:${port}`);
-})
+});
